560bd4e75711609e09a5f966d7dfaab6e604f8cc
blank <nickgarner2050@gmail.com>
2018-04-13 10:23:06 -0400

    Create fixcent5repos.sh
    nothing fancy

---
+++ fixcent5repos.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+#fix depreciated centos 5 repos so yum will flow
+cp /etc/yum.repos.d/CentOS-Base.repo /root/CentOS-Base.repo.bk
+sed -i 's/.*baseurl.*/baseurl=http:\/\/vault.centos.org\/5.11\/os\/i386\//g' /etc/yum.repos.d/CentOS-Base.repo
+
+
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Base.repo
