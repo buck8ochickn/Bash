5e54800159532823dd6756ef1425b6c80468ebe5
blank <nickgarner2050@gmail.com>
2018-04-13 11:18:30 -0400

    Update fixcent5repos.sh
    fixed error of over matching

--- fixcent5repos.sh
+++ fixcent5repos.sh
@@ -2,7 +2,7 @@
 #fix linebreaks
 #fix depreciated centos 5 repos so yum will flow
 cp /etc/yum.repos.d/CentOS-Base.repo /root/CentOS-Base.repo.bk
-sed -i 's/.*baseurl.*/baseurl=http:\/\/vault.centos.org\/5.11\/os\/i386\//g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i --regexp-extended 's/#?baseurl.*/baseurl=http:\/\/vault.centos.org\/5.11\/os\/i386\//g' /etc/yum.repos.d/CentOS-Base.repo
 
 
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-Base.repo
